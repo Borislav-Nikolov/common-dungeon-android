162827192535908fc88918ee7f3ff1f782711aad
Borislav <borislav.nikolov@pcloud.com>
2025-11-30 13:52:31 +0200

    Handle key store exceptions

--- data/storage/src/main/kotlin/com/commondnd/data/storage/KeyStoreUtils.kt
+++ data/storage/src/main/kotlin/com/commondnd/data/storage/KeyStoreUtils.kt
@@ -1,8 +1,11 @@
 package com.commondnd.data.storage
 
 import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
 import android.security.keystore.KeyProperties
+import java.security.InvalidKeyException
 import java.security.KeyStore
+import java.security.UnrecoverableKeyException
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
@@ -12,11 +15,16 @@ private const val ANDROID_KEYSTORE = "AndroidKeyStore"
 private const val TRANSFORMATION = "AES/GCM/NoPadding"
 private const val GCM_TAG_LENGTH = 128
 
+// TODO: handle exception on security (e.g. pattern, finerprint) changes
 fun getOrCreateKey(keyAlias: String): SecretKey {
     val keyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply { load(null) }
 
-    keyStore.getEntry(keyAlias, null)?.let {
-        return (it as KeyStore.SecretKeyEntry).secretKey
+    try {
+        keyStore.getEntry(keyAlias, null)?.let {
+            return (it as KeyStore.SecretKeyEntry).secretKey
+        }
+    } catch (_: UnrecoverableKeyException) {
+        keyStore.deleteEntry(keyAlias)
     }
 
     val keyGenerator = KeyGenerator.getInstance(
@@ -52,12 +60,24 @@ fun decrypt(keyAlias: String, encryptedData: ByteArray): String {
     val iv = encryptedData.copyOfRange(0, 12)
     val ciphertext = encryptedData.copyOfRange(12, encryptedData.size)
 
-    val cipher = Cipher.getInstance(TRANSFORMATION)
-    cipher.init(
-        Cipher.DECRYPT_MODE,
-        getOrCreateKey(keyAlias),
-        GCMParameterSpec(GCM_TAG_LENGTH, iv)
-    )
+    return try {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        cipher.init(
+            Cipher.DECRYPT_MODE,
+            getOrCreateKey(keyAlias),
+            GCMParameterSpec(GCM_TAG_LENGTH, iv)
+        )
+        cipher.doFinal(ciphertext).toString(Charsets.UTF_8)
+    } catch (keyPermanentlyInvalidatedException: KeyPermanentlyInvalidatedException) {
+        deleteKey(keyAlias)
+        throw keyPermanentlyInvalidatedException
+    } catch (invalidKeyException: InvalidKeyException) {
+        deleteKey(keyAlias)
+        throw invalidKeyException
+    }
+}
 
-    return cipher.doFinal(ciphertext).toString(Charsets.UTF_8)
+fun deleteKey(keyAlias: String) {
+    val keyStore = KeyStore.getInstance(ANDROID_KEYSTORE).apply { load(null) }
+    keyStore.deleteEntry(keyAlias)
 }
