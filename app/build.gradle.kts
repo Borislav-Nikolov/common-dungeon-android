fa0ae3317d4b5d030b8dd0f48729cbb4e884a1e9
Borislav <borislav.nikolov@pcloud.com>
2026-01-10 15:06:04 +0200

    Change version and build numbers and add version name suffix for debug builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,13 +11,16 @@ android {
 
     defaultConfig {
         applicationId = "com.commondnd"
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 1
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
+        debug {
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = true
             proguardFiles(
